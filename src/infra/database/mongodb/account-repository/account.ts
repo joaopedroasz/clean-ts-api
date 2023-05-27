6344815a834f4a0e8ad5b2b42e0e9cb0589fd544
João Pedro <joao.pedroasz00@gmail.com>
2023-05-27 17:28:31 -0300

    feat: ensure mongodb reconnects if client is down

--- src/infra/database/mongodb/account-repository/account.ts
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -1,4 +1,4 @@
-import { type Collection, type ObjectId } from 'mongodb'
+import { type ObjectId } from 'mongodb'
 import { type AddAccountRepository } from '../../../../data/protocols'
 import { type AccountModel } from '../../../../domain/models'
 import { type AddAccountModel } from '../../../../domain/use-cases'
@@ -12,14 +12,9 @@ interface AccountDocument {
 }
 
 export class AccountMongoRepository implements AddAccountRepository {
-  private readonly accountCollection: Collection<AddAccountModel>
-
-  constructor () {
-    this.accountCollection = MongoHelper.getCollection<AddAccountModel>('accounts')
-  }
-
   public async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const { insertedId } = await this.accountCollection.insertOne(accountData)
+    const accountCollection = await MongoHelper.getCollection<AddAccountModel>('accounts')
+    const { insertedId } = await accountCollection.insertOne(accountData)
     return MongoHelper.removeMongoId<AccountDocument>({ ...accountData, _id: insertedId })
   }
 }
--- src/infra/database/mongodb/helpers/mongo.ts
+++ src/infra/database/mongodb/helpers/mongo.ts
@@ -6,16 +6,17 @@ type DataWithId<Data> = { id: string } & Omit<Data, '_id'>
 
 export const MongoHelper = {
   client: undefined as unknown as MongoClient | undefined,
+  uri: undefined as unknown as string | undefined,
   connect: async (uri: string): Promise<void> => {
     MongoHelper.client = await MongoClient.connect(uri)
+    MongoHelper.uri = uri
   },
   disconnect: async (): Promise<void> => {
     await MongoHelper.client?.close()
+    MongoHelper.client = undefined
   },
-  getCollection: <CollectionType extends Document>(name: string): Collection<CollectionType> => {
-    if (!MongoHelper.client) {
-      throw new Error('MongoDB not connected')
-    }
+  getCollection: async <CollectionType extends Document>(name: string): Promise<Collection<CollectionType>> => {
+    if (!MongoHelper.client) MongoHelper.client = await MongoClient.connect(MongoHelper.uri as string)
     return MongoHelper.client.db().collection(name)
   },
   removeMongoId: <T>(collection: DataWithMongoId<T>): DataWithId<T> => {
