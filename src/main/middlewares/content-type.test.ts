3cb24e129a3aedeb8db51fd47fc7731e19549497
João Pedro <joao.pedroasz00@gmail.com>
2023-05-27 14:58:23 -0300

    test: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  it('should return default content type as json', async () => {
+    app.get('/test_content_type_json', (req, res) => {
+      res.send()
+    })
+
+    const response = await request(app).get('/test_content_type_json')
+
+    expect(response.headers['content-type']).toContain('json')
+  })
+
+  it('should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+
+    const response = await request(app).get('/test_content_type_xml')
+
+    expect(response.headers['content-type']).toContain('xml')
+  })
+})
