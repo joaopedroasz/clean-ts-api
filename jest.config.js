9ee6eac04c50a1c54a94373aab888ab7dc72cd20
João Pedro <joao.pedroasz00@gmail.com>
2023-05-27 17:11:00 -0300

    chore: ignore main and protocols in jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*protocols.ts', '!**/protocols/**'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest'
