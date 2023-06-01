4ec59d643cf8facf901e68788e85c17484859fa2
João Pedro <joao.pedroasz00@gmail.com>
2023-05-31 22:37:32 -0300

    feat: ensure expressRoute ends request after send response

--- src/main/adapters/express-route.ts
+++ src/main/adapters/express-route.ts
@@ -8,8 +8,11 @@ export const adaptExpressRoute = async (controller: Controller, req: Request, re
   const httpResponse = await controller.handle(httpRequest)
   if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
     res.status(httpResponse.statusCode).json(httpResponse.body)
+    res.end()
+    return
   }
   res.status(httpResponse.statusCode).json({
     error: httpResponse.body.message
   })
+  res.end()
 }
