1654fa92ea309a1003473faa54387f2c8ebf048d
João Pedro <joao.pedroasz00@gmail.com>
2023-06-04 16:59:47 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,19 +1,28 @@
 import { type AuthenticationModel, type Authentication } from '../../../domain/use-cases'
-import { type HashCompare, type LoadAccountByEmailRepository } from '../../protocols'
+import { type TokenGenerator, type HashCompare, type LoadAccountByEmailRepository } from '../../protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashCompare: HashCompare
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashCompare: HashCompare) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashCompare: HashCompare,
+    tokenGenerator: TokenGenerator
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashCompare = hashCompare
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth ({ email, password }: AuthenticationModel): Promise<string | undefined> {
     const account = await this.loadAccountByEmailRepository.load(email)
     if (!account) return undefined
 
-    await this.hashCompare.compare({ plainText: password, hash: account.password })
+    const isValidPassword = await this.hashCompare.compare({ plainText: password, hash: account.password })
+    if (!isValidPassword) return undefined
+
+    await this.tokenGenerator.generate(account.id)
   }
 }
