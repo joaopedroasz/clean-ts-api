4b8c9f7e9c67c46ae685a68f253d4d37f0e105be
João Pedro <joao.pedroasz00@gmail.com>
2023-06-08 11:02:49 -0300

    chore: ignore index files when test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*protocols.ts', '!**/protocols/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*protocols.ts', '!**/protocols/**', '!**/**/index.ts'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest'
