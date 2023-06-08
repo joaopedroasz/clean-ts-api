faf79e0609c18cd6ca2c0bddef2b0e60257ba13c
João Pedro <joao.pedroasz00@gmail.com>
2023-06-07 21:44:13 -0300

    feat: ensure AccountMongoRepository returns an account when load by email

--- src/infra/database/mongodb/account-repository/account.ts
+++ src/infra/database/mongodb/account-repository/account.ts
@@ -1,5 +1,5 @@
 import { type ObjectId } from 'mongodb'
-import { type AddAccountRepository } from '../../../../data/protocols'
+import { type LoadAccountByEmailRepository, type AddAccountRepository } from '../../../../data/protocols'
 import { type AccountModel } from '../../../../domain/models'
 import { type AddAccountModel } from '../../../../domain/use-cases'
 import { MongoHelper } from '../helpers'
@@ -11,10 +11,16 @@ interface AccountDocument {
   password: string
 }
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   public async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection<AddAccountModel>('accounts')
     const { insertedId } = await accountCollection.insertOne(accountData)
     return MongoHelper.removeMongoId<AccountDocument>({ ...accountData, _id: insertedId })
+  }
+
+  public async loadByEmail (email: string): Promise<AccountModel | undefined> {
+    const accountCollection = await MongoHelper.getCollection<AddAccountModel>('accounts')
+    const account = await accountCollection.findOne({ email })
+    return MongoHelper.removeMongoId<AccountDocument>(account)
   }
 }
