7f020a48f7216fd3f3b3372154df34025d374237
João Pedro <joao.pedroasz00@gmail.com>
2023-06-14 19:38:23 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

--- src/presentation/controller/survey/add-survey/add-survey.ts
+++ src/presentation/controller/survey/add-survey/add-survey.ts
@@ -3,14 +3,24 @@ import {
   type Controller,
   type HttpRequest,
   type HttpResponse,
-  badRequest
+  badRequest,
+  type AddSurvey
 } from './protocols'
 
 export class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const validationError = this.validation.validate(httpRequest.body)
     if (validationError) return badRequest(validationError)
+
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      answers,
+      question
+    })
   }
 }
--- src/presentation/controller/survey/add-survey/protocols.ts
+++ src/presentation/controller/survey/add-survey/protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../protocols'
 export * from '../../../protocols/validation'
 export * from '../../../helpers/http/http'
+export * from '../../../../domain/use-cases'
