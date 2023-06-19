8db6bda31d7d51f35743425b1df5cb89fd20ca2b
João Pedro <joao.pedroasz00@gmail.com>
2023-06-19 18:36:36 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct access token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,14 +1,47 @@
 import { forbidden } from '../helpers/http/http'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { type AccountModel } from '../../domain/models'
+import { type LoadAccountByTokenModel, type LoadAccountByToken } from '../../domain/use-cases'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  email: 'valid_email',
+  name: 'valid_name',
+  password: 'hashed_password'
+})
 
 describe('Auth Middleware', () => {
   it('should return 403 if no x-access-token existes in headers', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (data: LoadAccountByTokenModel): Promise<AccountModel | undefined> {
+        return makeFakeAccount()
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({
       headers: {}
     })
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('should call LoadAccountByToken with correct accessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (data: LoadAccountByTokenModel): Promise<AccountModel | undefined> {
+        return makeFakeAccount()
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
