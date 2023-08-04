1c479150de7a3605d9f113a6f2b6e7c5767ede97
João Pedro <joao.pedroasz00@gmail.com>
2023-08-04 11:40:23 -0300

    refactor: add collection name as class property

--- src/infra/database/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/database/mongodb/survey/survey-mongo-repository.ts
@@ -15,13 +15,15 @@ export interface SurveyDocument {
 }
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+  private readonly collectionName = 'surveys'
+
   public async add (data: AddSurveyModel): Promise<void> {
-    const surveyCollection = await MongoHelper.getCollection<SurveyDocument>('surveys')
+    const surveyCollection = await MongoHelper.getCollection<SurveyDocument>(this.collectionName)
     await surveyCollection.insertOne(data)
   }
 
   public async load (): Promise<SurveyModel[]> {
-    const surveyCollection = await MongoHelper.getCollection<SurveyDocument>('surveys')
+    const surveyCollection = await MongoHelper.getCollection<SurveyDocument>(this.collectionName)
     const surveys = await surveyCollection.find().toArray()
     return MongoHelper.removeManyMongoIds(surveys)
   }
