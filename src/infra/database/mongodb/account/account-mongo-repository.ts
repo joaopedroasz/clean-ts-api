cf27d6f8d0fc2dfff36b438c6312fcdbec3ee54c
João Pedro <joao.pedroasz00@gmail.com>
2023-08-04 11:44:31 -0300

    --amend

--- src/infra/database/mongodb/account/account-mongo-repository.ts
+++ src/infra/database/mongodb/account/account-mongo-repository.ts
@@ -20,26 +20,28 @@ export interface AccountDocument {
 }
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
+  private readonly collectionName = 'accounts'
+
   public async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = await MongoHelper.getCollection<AccountDocument>('accounts')
+    const accountCollection = await MongoHelper.getCollection<AccountDocument>(this.collectionName)
     const { insertedId } = await accountCollection.insertOne(accountData)
     return MongoHelper.removeMongoId<AccountDocument>({ ...accountData, _id: insertedId })
   }
 
   public async loadByEmail (email: string): Promise<AccountModel | undefined> {
-    const accountCollection = await MongoHelper.getCollection<AccountDocument>('accounts')
+    const accountCollection = await MongoHelper.getCollection<AccountDocument>(this.collectionName)
     const account = await accountCollection.findOne({ email })
     if (!account) return undefined
     return MongoHelper.removeMongoId<AccountDocument>(account)
   }
 
   public async updateAccessToken ({ id, token }: UpdateAccessTokenInput): Promise<void> {
-    const accountCollection = await MongoHelper.getCollection<AccountDocument>('accounts')
+    const accountCollection = await MongoHelper.getCollection<AccountDocument>(this.collectionName)
     await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { accessToken: token } })
   }
 
   public async loadByToken ({ token, role }: LoadAccountByTokenInput): Promise<AccountModel | undefined> {
-    const accountCollection = await MongoHelper.getCollection<AccountDocument>('accounts')
+    const accountCollection = await MongoHelper.getCollection<AccountDocument>(this.collectionName)
     const account = await accountCollection.findOne({
       accessToken: token,
       $or: [{
