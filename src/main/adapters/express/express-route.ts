e0d4ba7fa88bdbf97127c8686eccd3a47a5a25a7
João Pedro <joao.pedroasz00@gmail.com>
2024-03-15 15:32:30 -0300

    refactor: add params and account id in express route

--- src/main/adapters/express/express-route.ts
+++ src/main/adapters/express/express-route.ts
@@ -5,7 +5,9 @@ import { type Controller, type HttpRequest } from '@/presentation/protocols'
 export const adaptExpressRoute = (controller: Controller): RequestHandler => {
   return async (req: Request, res: Response): Promise<void> => {
     const httpRequest: HttpRequest = {
-      body: req.body
+      body: req.body,
+      params: req.params,
+      accountId: req.accountId
     }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
