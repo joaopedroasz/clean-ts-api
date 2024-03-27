d1f32c794e9b39a31a2f98e6108304c3d34cf914
João Pedro <joao.pedroasz00@gmail.com>
2024-03-27 14:41:36 -0300

    feat: apply n-cache middleware on swagger route

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -2,7 +2,8 @@ import swaggerUi from 'swagger-ui-express'
 import { type Application } from 'express'
 
 import swaggerConfig from '../docs'
+import { noCache } from '../middlewares'
 
 export default (app: Application): void => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
+  app.use('/api-docs', noCache, swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 }
