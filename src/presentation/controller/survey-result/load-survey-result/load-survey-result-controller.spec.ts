4e47b9fa89851b1bbcd9de3133259e5c2421b99b
João Pedro <joao.pedroasz00@gmail.com>
2024-04-17 20:19:54 -0300

    test: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns undefined

--- src/presentation/controller/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controller/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,6 +1,8 @@
+import { forbidden } from '@/presentation/helpers/http/http'
+import { mockLoadSurveyById } from '@/presentation/test'
+import { InvalidParamError } from '@/presentation/errors'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import type { HttpRequest, LoadSurveyById } from './protocols'
-import { mockLoadSurveyById } from '@/presentation/test'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -31,5 +33,14 @@ describe('LoadSurveyResult Controller', () => {
     await sut.handle(makeFakeRequest())
 
     expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  it('should return 403 if LoadSurveyById returns undefined', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockResolvedValueOnce(undefined)
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
